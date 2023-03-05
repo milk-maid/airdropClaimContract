f1cdc31e0618fcf36bcb8f11b14cfa46316f277f
milk-maid <roqbell@gmail.com>
2023-03-05 02:21:18 +0100

    write the .sol file 'the begining of the end'

--- contracts/Airdrop.sol
+++ contracts/Airdrop.sol
@@ -1,9 +1,16 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 // Uncomment this line to use console.log
 // import "hardhat/console.sol";
 
-contract Airdrop {
-    
+contract Airdrop is ERC20 {
+  
+  string  name_;
+  string symbol_;
+  constructor(string memory _name, string memory _symbol) ERC20(_name, _symbol){
+    name_ = _name;
+    symbol_ = _symbol;
+  }
 }
